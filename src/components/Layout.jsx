087df8c77bb5285dd1ad9d83c7bc0fbb833c8470
HarpeBlue <hspe2507@gmail.com>
2020-08-11 22:57:10 -0500

    Footer y Card options primera versión

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import Header from './Header';
+import Footer from './Footer';
 import CardBasic from './CardBasic';
+import CardOption from './CardOption';
 
 function Layout (props) {
-    
     // const children = props.children;
     // retornar lo mismo que esta interno en el layout
     return (
@@ -13,7 +14,8 @@ function Layout (props) {
             <Header />
             {props.children}
             <CardBasic eventUrlImage='https://images.freeimages.com/images/small-previews/5c6/sunset-jungle-1383333.jpg' eventTitle='El llanerazo' eventDetails='Vive el mayor festival de música llanera' eventLocale='https://www.airbnb.com.co/rooms/20870347?location=granada&previous_page_section_name=1000&federated_search_id=26052e9b-59dc-4591-b857-ff2ea50a585b' />
-            {/* <Footer /> */}
+            <CardOption eventDetails='Plan 2 dias. Estadia opcional. wifi. Zona de lavado. Zona de camping. Desayuno y almuerzo' eventLocale='https://www.airbnb.com.co/rooms/20870347?location=granada&previous_page_section_name=1000&federated_search_id=26052e9b-59dc-4591-b857-ff2ea50a585b' eventTitle='El llanerazo' eventDisponibility='Con disponibilidad' pricePeople='100.000'  priceLocation='80.000' eventUrlImage='https://images.freeimages.com/images/small-previews/5c6/sunset-jungle-1383333.jpg' />
+            <Footer />
         </React.Fragment>
     )
 }
