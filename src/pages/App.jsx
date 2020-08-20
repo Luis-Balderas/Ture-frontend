d1438c190192067565f03dd123687d117e4c4e3b
masmelacarol <carolstefannym@gmail.com>
2020-08-20 16:07:10 -0500

    [upd]: se agrega nueva ruta y se colocan estilos globales

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from '../components/Layout';
 import NotFound from '../components/NotFound';
+import EventDetails from './EventDetails';
 import Home from './Home';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/event' component={EventDetails} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default App;
