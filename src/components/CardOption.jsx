a30966f0a3e3a390ab778b30dac845f765d49d07
Harold Peñaloza <hspe2507@gmail.com>
2020-08-26 14:24:16 -0500

    Se revisó y agregó HTML semántico.

--- src/components/CardOption.jsx
+++ src/components/CardOption.jsx
@@ -7,11 +7,13 @@ const CardOption = ({ item }) => {
   const { eventUrlImage, priceLocation, pricePeople, eventDisponibility, eventTitle, eventLocale, eventDetails } = item;
   return (
     <div className='cardOption'>
+
       <div className='cardOption__container'>
         <a className='cardOption__link' href={eventLocale}>
           <img src={eventUrlImage} alt='Imagen de Evento' />
         </a>
         <div className='cardOption__details'>
+
           <div className='cardOption__details--left'>
             <p>
               <small>Estadía opcional ({eventDisponibility})</small>
@@ -20,142 +22,49 @@ const CardOption = ({ item }) => {
               <h5>{eventTitle}</h5>
             </a>
           </div>
-          <img src={IconHeart} alt='icono de favoritos.' />
+
+          <figure>
+            <img src={IconHeart} alt='icono de favoritos.' />
+          </figure>
           <div className='cardOption__details--center'>
             <p>{eventDetails}</p>
           </div>
+
           <div className='cardOption__details--right'>
+
             <div className='star-rating'>
               <figure>
                 <img src={IconStar} alt='Icono de calificación' />
               </figure>
-              <span> 5.0 (235) </span>
+              <small>5.0 <span>(235)</span></small>
             </div>
-            <div>
+            <div className='price'>
               {pricePeople ? (
-                <h3>
+                <p>
                   <span>
                     {pricePeople}
                     COP
                   </span>
                   por persona
-                </h3>
+                </p>
               ) : null}
               {priceLocation ? (
-                <h3>
+                <p>
                   <span>
                     {priceLocation}
                     COP
                   </span>
                   (Estadía opcional)
-                </h3>
+                </p>
               ) : null}
             </div>
-            
+
           </div>
         </div>
       </div>
 
-
-
-      <div className='cardOption__container'>
-        <a className='cardOption__link' href={eventLocale}>
-          <img src={eventUrlImage} alt='Imagen de Evento' />
-        </a>
-        <div className='cardOption__details'>
-          <div className='cardOption__details--left'>
-            <p>
-              <small>Estadía opcional ({eventDisponibility})</small>
-            </p>
-            <a href={eventLocale}>
-              <h5>{eventTitle}</h5>
-            </a>
-          </div>
-          <img src={IconHeart} alt='icono de favoritos.' />
-          <div className='cardOption__details--center'>
-            <p>{eventDetails}</p>
-          </div>
-          <div className='cardOption__details--right'>
-            <div className='star-rating'>
-              <figure>
-                <img src={IconStar} alt='Icono de calificación' />
-              </figure>
-              <span> 5.0 (235) </span>
-            </div>
-            <div>
-              {pricePeople ? (
-                <h3>
-                  <span>
-                    {pricePeople}
-                    COP
-                  </span>
-                  por persona
-                </h3>
-              ) : null}
-              {priceLocation ? (
-                <h3>
-                  <span>
-                    {priceLocation}
-                    COP
-                  </span>
-                  (Estadía opcional)
-                </h3>
-              ) : null}
-            </div>
-            
-          </div>
-        </div>
-      </div>
-      <div className='cardOption__container'>
-        <a className='cardOption__link' href={eventLocale}>
-          <img src={eventUrlImage} alt='Imagen de Evento' />
-        </a>
-        <div className='cardOption__details'>
-          <div className='cardOption__details--left'>
-            <p>
-              <small>Estadía opcional ({eventDisponibility})</small>
-            </p>
-            <a href={eventLocale}>
-              <h5>{eventTitle}</h5>
-            </a>
-          </div>
-          <img src={IconHeart} alt='icono de favoritos.' />
-          <div className='cardOption__details--center'>
-            <p>{eventDetails}</p>
-          </div>
-          <div className='cardOption__details--right'>
-            <div className='star-rating'>
-              <figure>
-                <img src={IconStar} alt='Icono de calificación' />
-              </figure>
-              <span> 5.0 (235) </span>
-            </div>
-            <div>
-              {pricePeople ? (
-                <h3>
-                  <span>
-                    {pricePeople}
-                    COP
-                  </span>
-                  por persona
-                </h3>
-              ) : null}
-              {priceLocation ? (
-                <h3>
-                  <span>
-                    {priceLocation}
-                    COP
-                  </span>
-                  (Estadía opcional)
-                </h3>
-              ) : null}
-            </div>
-            
-          </div>
-        </div>
-      </div>
     </div>
-    
+
   );
 };
 
