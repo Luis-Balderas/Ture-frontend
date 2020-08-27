ab6383b8c16778886895def5a0a8e8c01ec93bd0
brahianf <brahianf@unicauca.edu.co>
2020-08-26 19:29:35 -0500

    Correción BEM

--- src/components/PayDetails.jsx
+++ src/components/PayDetails.jsx
@@ -1,33 +1,51 @@
 import React from 'react';
 import '../assets/styles/components/PayDetails.scss';
 
-import PayDetailsItem from './PayDetailsItem';
+// import PayDetailsItem from './PayDetailsItem';
 
 const PayDetails = () => {
-   const data = [
-    {
-      label1: 'Medio de Pago',
-      label2: 'Nombre',
-      label3: 'Apellido',
-      label4: 'Informacion de facturación',
-      label5: 'Pais/Región',
-      label6: 'Pago único',
-      label7: 'Pago fraccionado',
-      text7: 'Paga $253,200 Ahora y el resto($168,800) se te cobrará automáticamente a la misma forma de pago el 12 de dic de 2020, sin tarifas adicionales'
-    },
-    {
-      labelplaceholder1: 'Crédito',
-      labelplaceholder2: 'Nombre',
-      labelplaceholder3: 'apellido',
-      labelplaceholder4: 'codigo Postal',
-      labelplaceholder5: 'Colombia'
-    },
-  ];
 
   return (
-    <section className='PayDetails'>
-        <span><h4>Confirmar y Pagar</h4></span>
-        <PayDetailsItem data={data[0]}/>
+    <section className='Reservation__PayDetails'>
+        <span><h5><strong>Confirmar y Pagar</strong></h5></span>
+        <div className='Reservation__PayDetails--items'>
+        <div className='Reservation__PayDetails--pay'>
+           <p>Medio de Pago</p>
+           <form><input type="text" placeholder="Tarjeta Débito"></input></form>
+        </div>
+        <div>
+          <p>Nombre</p>
+          <form><input type="text" placeholder="Nombre"></input></form>
+        </div>
+        <div>
+          <p>Apellido</p>
+          <form><input type="text" placeholder="Apellido"></input></form>
+        </div>
+        <div>
+          <p>Informacion de facturación</p>
+          <form><input type="text" placeholder="Código Postal"></input></form>
+        </div>
+        <div>
+          <p>Pais/Región</p>
+          <form><input type="text" placeholder="Colombia"></input></form>
+        </div>
+        <div  className='Reservation__PayDetails--pay1'>
+          <label><input name="isGoing" type="checkbox"  disabled={true} checked={true}/><strong>Pago único</strong></label>
+          <p>$442.000</p>
+        </div>
+        <div  className='Reservation__PayDetails--pay2'>
+         <label><input name="isGoing" type="checkbox"  disabled={true} checked={false} /><strong>Pago fraccionado</strong></label>
+          <p>Paga $253,200 Ahora y el resto($168,800) se te cobrará automáticamente a la misma forma de pago el 12 de dic de 2020, sin tarifas adicionales</p>
+        </div>
+        <div className="Reservation__PayDetails--button">
+          <button type='button'>
+            <p>Cancelar</p>
+          </button>
+          <button type='button'>
+            <p>Confirmar</p>
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
